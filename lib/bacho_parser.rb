46eef85bb779479ec18ea405272cc8e768623a36
Vincent Boucheny <vincent.boucheny@fluxfederation.com>
2019-03-15 12:43:15 +1300

    Vanilla gem

---
+++ lib/bacho_parser.rb
@@ -0,0 +1,6 @@
+require "bacho_parser/version"
+
+module BachoParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bacho_parser_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BachoParser do
+  it "has a version number" do
+    expect(BachoParser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
